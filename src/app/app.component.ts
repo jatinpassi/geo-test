62747e1a1de14e2a600679acebf0f4e7f23e3dfd
jatinpassi <jatinpassi111@gmail.com>
2020-08-16 16:45:35 +0530

    first setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'geotest';
+  style = {
+    sources: {
+      world: {
+        type: "geojson",
+        data:
+          "https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json",
+      },
+    },
+    version: 8,
+    layers: [
+      {
+        id: "countries",
+        type: "fill",
+        source: "world",
+        layout: {},
+        paint: {
+          "fill-color": "#6F788A",
+        },
+      },
+    ],
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { NgxMapboxGLModule } from "ngx-mapbox-gl";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxMapboxGLModule.withConfig({
+      accessToken:
+        "pk.eyJ1IjoiamF0aW5wYXNzaTExMSIsImEiOiJja2RiZXJ0NHUwcmRwMnR0OW1mbGdmamRsIn0.ZGZM94VjRMCA1qGc6DK6dQ", // Optional, can also be set per map (accessToken input of mgl-map)
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
